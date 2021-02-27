d2a44107f5e79d068ed2061a00b6600259a51617
sky3107 <sky3107@gmail.com>
2021-02-27 11:55:12 +0200

    Init priject

---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Index from '../pages'
+
+describe('Index pages', () => {
+  it('Should return Index pages', () => {
+    const wrapper = shallow(<Index />)
+    expect(wrapper.find('div')).toHaveLength(1)
+  })
+})
